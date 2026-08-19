963338accd4e0b7642663f19be26064b5ab205a2
Amar Mantry <mantryamar@gmail.com>
2026-08-19 18:28:47 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stack=new ArrayDeque<>();
+        HashMap<Character,Character> check=new HashMap<>();
+        check.put('}','{');
+        check.put(')','(');
+        check.put(']','[');
+        for(char c : s.toCharArray()){
+            if(c=='(' || c=='{' || c=='[') {stack.push(c);}
+            else{
+                if(stack.isEmpty() || stack.pop()!=check.get(c)) {
+                    return false;
+                }
+                // else{
+                //     stack.pop();
+                // }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
